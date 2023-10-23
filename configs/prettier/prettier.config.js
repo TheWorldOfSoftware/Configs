9a5cf23f9246e542b39f78de3a17d7316ee0dfcd
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-10-23 08:34:45 +0200

    Added prettier config
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

---
+++ configs/prettier/prettier.config.js
@@ -0,0 +1,19 @@
+export default {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "strict",
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "never",
+  quoteProps: "as-needed",
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false,
+  vueIndentScriptAndStyle: false
+};
