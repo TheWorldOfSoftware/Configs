f91ad53723992049213ed9356b9e5e1750ba8090
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2024-01-15 23:24:33 +0100

    chore: Added import plugin rules not included in recommended
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- configs/eslint/plugins/import.config.js
+++ configs/eslint/plugins/import.config.js
@@ -17,14 +17,12 @@ export const importPluginTypescript = {
     import: importPlugin
   },
   settings: {
-    "import/extensions": [...jsExtensions, ...tsExtensions],
+    "import/extensions": tsExtensions,
     "import/parsers": {
-      espree: jsExtensions,
       "@typescript-eslint/parser": tsExtensions
     },
     "import/resolver": {
-      typescript: true,
-      node: true
+      typescript: true
     }
   },
   rules: {
@@ -52,6 +50,77 @@ export default {
     }
   },
   rules: {
-    ...importPlugin.configs.recommended.rules
+    ...importPlugin.configs.recommended.rules,
+    // Helpful warnings
+    "import/no-deprecated": "warn",
+    "import/no-empty-named-blocks": "warn",
+    "import/no-extraneous-dependencies": ["warn", { devDependencies: false }],
+    "import/no-mutable-exports": "warn",
+    "import/no-unused-modules": ["warn", { missingExports: true }],
+
+    // Module systems
+    "import/no-amd": "error",
+    "import/no-commonjs": "error",
+    "import/no-import-module-exports": "error",
+    "import/no-nodejs-modules": "off",
+    "import/unambiguous": "error",
+
+    // Static analysis
+    "import/no-absolute-path": "error",
+    "import/no-cycle": "error",
+    "import/no-dynamic-require": "error",
+    "import/no-internal-modules": ["error", { allow: ["**/util/**/*"] }],
+    "import/no-relative-packages": "error",
+    "import/no-relative-parent-imports": "error",
+    "import/no-restricted-paths": [
+      "error",
+      {
+        zones: [
+          {
+            target: "src/*/**/*",
+            from: "src/*",
+            message: "Do not access application files from logic!"
+          },
+          {
+            target: "src/features/**/*",
+            from: "src/routes/**/*",
+            message: "Do not access routes from feature functionality!"
+          },
+          {
+            target: "src/lib/**/*",
+            from: "src/!(lib)/**/*",
+            message: "Lib files cannot access application logic!"
+          },
+          {
+            target: "src/models/**/*",
+            from: [
+              "src/features/**/*",
+              "src/repositories/**/*",
+              "src/routes/**/*"
+            ],
+            message: "Do not access logic from a data model!"
+          },
+          {
+            target: "src/repositories/**/*",
+            from: ["src/features/**/*", "src/routes/**/*"],
+            message: "Do not access logic from a repository!"
+          },
+          {
+            target: "src/routes/**/*",
+            from: "src/repositories/**/*",
+            message: "Do not access repositories directly!"
+          },
+          {
+            target: "src/types/**/*",
+            from: "src/**/*",
+            except: ["src/models/**/*", "src/types/**/*"],
+            message: "Do not access logic from type definitions!"
+          }
+        ]
+      }
+    ],
+    "import/no-self-import": "error",
+    "import/no-useless-path-segments": "error",
+    "import/no-webpack-loader-syntax": "error"
   }
 };
